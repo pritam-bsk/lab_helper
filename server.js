9b3e01fc5eec3be25e978e2f381bfa1aa1894d2c
pritam-bsk <riju.coder@gmail.com>
2026-08-19 22:36:13 +0530

    lab helper code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const fs = require('node:fs');
+
+let code = '';
+
+fs.readFile('code.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    code = data;
+    console.log(data);
+});
+
+
+app.post('/', (req, res) => {
+    res.send(code);
+});
+
+app.listen(3000, () => console.log('Express server running on port 3000'));
